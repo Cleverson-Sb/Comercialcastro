ff8bd117be8e85efde4977c48b0a06148e284b09
Cleverson Marciel da Silva Brandão <105869524+Cleverson-Sb@users.noreply.github.com>
2023-03-22 08:10:24 -0300

    att

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobilimenu');
     if (menuMobile.classList.contains('open') ){
         (menuMobile.classList.remove('open'));
-        document.querySelector('.icon') = <i class="fa-solid fa-bars"></i>
     } else {
         (menuMobile.classList.add('open'))
-        document.querySelector('.icon') = <i class="fa-solid fa-x"></i>
     }
 }
