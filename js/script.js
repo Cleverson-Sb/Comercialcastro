420e740cf69f11a1f4e1d19a6ff90d697af5983a
Cleverson Marciel da Silva Brandão <cleversonmarcielsb@gmail.com>
2023-04-10 09:11:19 -0300

    att

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobilimenu');
     if (menuMobile.classList.contains('open') ){
         (menuMobile.classList.remove('open'));
+        document.querySelector('.icone').src = "img/menu_white_36dp.svg";
     } else {
         (menuMobile.classList.add('open'))
+        document.querySelector('.icone').src = "img/close_white_36dp.svg";
     }
 }
 
